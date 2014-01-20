42db4af566f6f712fb913cb5e5d317a7c209f56f
Kang Hyojun <hyojun@admire.kr>
2014-01-20 10:54:57 +0900

    Add sc2web

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
+dependencies = [
+    # alask == 0.0.1
+    'http://admire93.github.io/alask/alask-0.0.1.tar.gz'
+]
+
 setup(name='sc2statistics',
       version='0.0.1',
       author='Kang Hyojun',
       author_email='hyojun@admire.kr',
       install_requires=[
-      ])
+          'sc2reader == 0.6.4', 'alask==0.0.1'
+      ],
+      dependency_links=dependencies)
