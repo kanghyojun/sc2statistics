9ff787c01236857768757cccc9538e22bb7ad3cd
Kang Hyojun <hyojun@admire.kr>
2014-01-20 10:29:38 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='sc2statistics',
+      version='0.0.1',
+      author='Kang Hyojun',
+      author_email='hyojun@admire.kr',
+      install_requires=[
+      ])
